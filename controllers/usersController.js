38b77c8670a55f7280f88f1bd6f39dbaf7863b9e
jdvpl <juanda554242@gmail.com>
2021-09-09 21:16:26 -0500

    formulario listo

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,6 +1,8 @@
 const User = require('../models/user');
+const Rol = require('../models/rol');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
+const storage = require('../utils/cloud_storage');
 
 module.exports = {
 
@@ -19,15 +21,71 @@ module.exports = {
         }
     },
 
+    async findById(req, res, next) {
+        try {
+            const id = req.params.id;
+
+            const data = await User.findByUserId(id);    
+            console.log(`Usuario: ${data}`);
+            return res.status(201).json(data);
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al obtener el usuario por ID'
+            });
+        }
+    },
+    
+    async findDeliveryMen(req, res, next) {
+        try {
+            const data = await User.findDeliveryMen();    
+            console.log(`Repartidores: ${data}`);
+            return res.status(201).json(data);
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al obtener los repartidores'
+            });
+        }
+    },
+    
+    async getAdminsNotificationTokens(req, res, next) {
+        try {
+            const data = await User.getAdminsNotificationTokens();    
+            let tokens = [];
+
+
+            data.forEach(d => {
+                tokens.push(d.notification_token);
+            });
+
+            console.log('Tokens de admin:', tokens);
+            return res.status(201).json(tokens);
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al obtener los repartidores'
+            });
+        }
+    },
+
     async register(req, res, next) {
         try {
             
             const user = req.body;
             const data = await User.create(user);
 
+            await Rol.create(data.id, 1); // ROL POR DEFECTO (CLIENTE)
+
             return res.status(201).json({
                 success: true,
-                message: 'El registro se realizo correctamente',
+                message: 'El registro se realizo correctamente, ahora inicia sesion',
                 data: data.id
             });
 
@@ -37,6 +95,103 @@ module.exports = {
             return res.status(501).json({
                 success: false,
                 message: 'Hubo un error con el registro del usuario',
+                error: error
+            });
+        }
+    },
+
+    async registerWithImage(req, res, next) {
+        try {
+            
+            const user = JSON.parse(req.body.user);
+            console.log(`Datos enviados del usuario: ${user}`);
+
+            const files = req.files;
+
+            if (files.length > 0) {
+                const pathImage = `image_${Date.now()}`; // NOMBRE DEL ARCHIVO
+                const url = await storage(files[0], pathImage);
+
+                if (url != undefined && url != null) {
+                    user.image = url;
+                }
+            }
+
+            const data = await User.create(user);
+
+            await Rol.create(data.id, 1); // ROL POR DEFECTO (CLIENTE)
+
+            return res.status(201).json({
+                success: true,
+                message: 'El registro se realizo correctamente, ahora inicia sesion',
+                data: data.id
+            });
+
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Hubo un error con el registro del usuario',
+                error: error
+            });
+        }
+    },
+
+    async update(req, res, next) {
+        try {
+            
+            const user = JSON.parse(req.body.user);
+            console.log(`Datos enviados del usuario: ${JSON.stringify(user)}`);
+
+            const files = req.files;
+
+            if (files.length > 0) {
+                const pathImage = `image_${Date.now()}`; // NOMBRE DEL ARCHIVO
+                const url = await storage(files[0], pathImage);
+
+                if (url != undefined && url != null) {
+                    user.image = url;
+                }
+            }
+
+            await User.update(user);
+
+            return res.status(201).json({
+                success: true,
+                message: 'Los datos del usuario se actualizaron correctamente'
+            });
+
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Hubo un error con la actualizacion de datos del usuario',
+                error: error
+            });
+        }
+    },
+    
+    async updateNotificationToken(req, res, next) {
+        try {
+            
+            const body = req.body;
+            console.log('Datos enviados del usuario: ', body);
+
+            await User.updateNotificationToken(body.id, body.notification_token);
+
+            return res.status(201).json({
+                success: true,
+                message: 'El token de notificaciones se ha almacenado correctamente'
+            });
+
+        } 
+        catch (error) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Hubo un error con la actualizacion de datos del usuario',
                 error: error
             });
         }
@@ -59,6 +214,7 @@ module.exports = {
             if (User.isPasswordMatched(password, myUser.password)) {
                 const token = jwt.sign({id: myUser.id, email: myUser.email}, keys.secretOrKey, {
                     // expiresIn: (60*60*24) // 1 HORA
+                    // expiresIn: (60 * 3) // 2 MINUTO
                 });
                 const data = {
                     id: myUser.id,
@@ -67,13 +223,18 @@ module.exports = {
                     email: myUser.email,
                     phone: myUser.phone,
                     image: myUser.image,
-                    session_token: `JWT ${token}`
+                    session_token: `JWT ${token}`,
+                    roles: myUser.roles
                 }
+                
+                await User.updateToken(myUser.id, `JWT ${token}`);
+
+                console.log(`USUARIO ENVIADO ${data}`);
 
                 return res.status(201).json({
                     success: true,
                     data: data,
-                    message:"El usario ha sido autenticado"
+                    message: 'El usuario ha sido autenticado'
                 });
             }
             else {
@@ -89,6 +250,25 @@ module.exports = {
             return res.status(501).json({
                 success: false,
                 message: 'Error al momento de hacer login',
+                error: error
+            });
+        }
+    },
+    async logout(req, res, next) {
+
+        try {
+            const id = req.body.id;
+            await User.updateToken(id, null);
+            return res.status(201).json({
+                success: true,
+                message: 'La sesion del usuario se ha cerrado correctamente'
+            });
+        } 
+        catch(e) {
+            console.log(`Error: ${error}`);
+            return res.status(501).json({
+                success: false,
+                message: 'Error al momento de cerrar sesion',
                 error: error
             });
         }
--- models/user.js
+++ models/user.js
@@ -35,23 +35,139 @@ User.findById = (id, callback) => {
 
 }
 
+User.findByUserId = (id) => {
+    const sql = `
+    SELECT
+        U.id,
+        U.email,
+        U.name,
+        U.lastname,
+        U.image,
+        U.phone,
+        U.password,
+        U.session_token,
+        U.notification_token,
+        json_agg(
+            json_build_object(
+                'id', R.id,
+                'name', R.name,
+                'image', R.image,
+                'route', R.route
+            )
+        ) AS roles
+    FROM 
+        users AS U
+    INNER JOIN
+        user_has_roles AS UHR
+    ON
+        UHR.id_user = U.id
+    INNER JOIN
+        roles AS R
+    ON
+        R.id = UHR.id_rol
+    WHERE
+        U.id = $1
+    GROUP BY
+        U.id
+    `
+    return db.oneOrNone(sql, id);
+}
+
+User.findDeliveryMen = () => {
+    const sql = `
+    SELECT
+        U.id,
+        U.email,
+        U.name,
+        U.lastname,
+        U.image,
+        U.phone,
+        U.password,
+        U.session_token,
+        U.notification_token
+    FROM
+        users AS U
+    INNER JOIN
+        user_has_roles AS UHR
+    ON 
+        UHR.id_user = U.id
+    INNER JOIN
+        roles AS R
+    ON
+        R.id = UHR.id_rol
+    WHERE
+        R.id = 3  
+    `;
+    return db.manyOrNone(sql);
+}
+
 User.findByEmail = (email) => {
     const sql = `
     SELECT
-        id,
-        email,
-        name,
-        lastname,
-        image,
-        phone,
-        password,
-        session_token
-    FROM
-        users
+        U.id,
+        U.email,
+        U.name,
+        U.lastname,
+        U.image,
+        U.phone,
+        U.password,
+        U.session_token,
+        json_agg(
+            json_build_object(
+                'id', R.id,
+                'name', R.name,
+                'image', R.image,
+                'route', R.route
+            )
+        ) AS roles
+    FROM 
+        users AS U
+    INNER JOIN
+        user_has_roles AS UHR
+    ON
+        UHR.id_user = U.id
+    INNER JOIN
+        roles AS R
+    ON
+        R.id = UHR.id_rol
     WHERE
-        email = $1
+        U.email = $1
+    GROUP BY
+        U.id
     `
     return db.oneOrNone(sql, email);
+}
+
+User.getAdminsNotificationTokens = () => {
+    const sql = `
+    SELECT
+        U.notification_token
+    FROM 
+        users AS U
+    INNER JOIN
+        user_has_roles AS UHR
+    ON
+        UHR.id_user = U.id
+    INNER JOIN
+        roles AS R
+    ON
+        R.id = UHR.id_rol
+    WHERE
+        R.id = 2
+    `
+    return db.manyOrNone(sql);
+}
+
+User.getUserNotificationToken = (id) => {
+    const sql = `
+    SELECT
+        U.notification_token
+    FROM 
+        users AS U
+    WHERE
+        U.id = $1
+    `
+    return db.oneOrNone(sql, id);
 }
 
 User.create = (user) => {
@@ -83,6 +199,62 @@ User.create = (user) => {
         user.password,
         new Date(),
         new Date()
+    ]);
+}
+
+User.update = (user) => {
+    const sql = `
+    UPDATE
+        users
+    SET
+        name = $2,
+        lastname = $3,
+        phone = $4,
+        image = $5,
+        updated_at = $6
+    WHERE
+        id = $1
+    `;
+
+    return db.none(sql, [
+        user.id,
+        user.name,
+        user.lastname,
+        user.phone,
+        user.image,
+        new Date()
+    ]);
+}
+
+User.updateToken = (id, token) => {
+    const sql = `
+    UPDATE
+        users
+    SET
+        session_token = $2
+    WHERE
+        id = $1
+    `;
+
+    return db.none(sql, [
+        id,
+        token
+    ]);
+}
+
+User.updateNotificationToken = (id, token) => {
+    const sql = `
+    UPDATE
+        users
+    SET
+        notification_token = $2
+    WHERE
+        id = $1
+    `;
+
+    return db.none(sql, [
+        id,
+        token
     ]);
 }
 
